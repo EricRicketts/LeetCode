e2bf09f36f098763495ac531fc1e49bfeb9e1559
EricRicketts <eric_ricketts@me.com>
2021-12-20 16:08:32 -0500

    skeleton for solution and tests

---
+++ java/easy/minimun-absolute-difference/src/main/java/Solution.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public List<List<Integer>> minimumAbsDifference(int[] arr) {
+        List<List<Integer>> minimumAbsDifferenceListOfPairs = new ArrayList<>();
+
+        return minimumAbsDifferenceListOfPairs;
+    }
+}
