59c5702110f6d8c5715df6c62de9ea4962104b55
EricRicketts <eric_ricketts@me.com>
2021-12-20 18:41:53 -0500

    first test failed but I need to get the code to the remote

--- java/easy/minimun-absolute-difference/src/main/java/Solution.java
+++ java/easy/minimun-absolute-difference/src/main/java/Solution.java
@@ -1,10 +1,54 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Solution {
     public List<List<Integer>> minimumAbsDifference(int[] arr) {
         List<List<Integer>> minimumAbsDifferenceListOfPairs = new ArrayList<>();
+        int lastIndex = arr.length - 1;
+        int[] miniDiffArray = new int[]{(int)Integer.MAX_VALUE};
 
+        for (int index = 0; index < lastIndex; index += 1) {
+            int[] arraySlice = Arrays.copyOfRange(arr, index, lastIndex + 1);
+            accumulateAbsDifferenceOfPairs(minimumAbsDifferenceListOfPairs, miniDiffArray, arraySlice);
+        }
+
+        sortListOfPairs(minimumAbsDifferenceListOfPairs);
         return minimumAbsDifferenceListOfPairs;
     }
+
+    private void accumulateAbsDifferenceOfPairs(List<List<Integer>> pairs, int[] miniDiffArray, int[] arraySlice) {
+        int firstValue = arraySlice[0];
+
+        for (int index = 1; index < arraySlice.length; index += 1) {
+            int secondValue = arraySlice[index];
+            int difference = Math.abs(secondValue - firstValue);
+            int currentMinDiff = miniDiffArray[0];
+            if (difference < currentMinDiff) {
+                miniDiffArray[0] = difference;
+                pairs.clear();
+                sortPairAndAddToList(pairs, firstValue, secondValue);
+            } else if (difference == currentMinDiff) {
+                sortPairAndAddToList(pairs, firstValue, secondValue);
+            } else {
+                continue;
+            }
+        }
+    }
+
+    private void sortListOfPairs(List<List<Integer>> pairs) {
+        Comparator<List<Integer>> comp = (a, b) -> {
+            return a.get(0).compareTo(b.get(0));
+        };
+        Collections.sort(pairs, comp);
+    }
+
+    private void sortPairAndAddToList(List<List<Integer>> pairs, int firstValue, int secondValue) {
+        Integer[] pair;
+        if (secondValue < firstValue) {
+            pair = new Integer[]{secondValue, firstValue};
+        } else {
+            pair = new Integer[]{firstValue, secondValue};
+        }
+        pairs.add(Arrays.asList(pair));
+    }
+
 }
--- java/easy/minimun-absolute-difference/src/test/java/SolutionTest.java
+++ java/easy/minimun-absolute-difference/src/test/java/SolutionTest.java
@@ -1,10 +1,28 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SolutionTest {
 
+    private List<List<Integer>> expected;
+    private Solution solution;
+
+    @BeforeEach
+    public void setUp() {
+        solution = new Solution();
+    }
+
     @Test
-    void minimumAbsDifference() {
+    public void minimumAbsDifference() {
+        Integer[][] expectedArray = new Integer[][]{{1, 2}, {2, 3}, {3, 4}};
+        for (Integer[] subArray:expectedArray) {
+            expected.add(Arrays.asList(subArray));
+        }
+        int[] inputArray = new int[]{4, 2, 1, 3};
+        assertEquals(expected, solution.minimumAbsDifference(inputArray));
     }
 }
