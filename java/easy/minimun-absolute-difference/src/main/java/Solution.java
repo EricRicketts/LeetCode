b97f57879561f57a99a606c9d1c5a7f71a4439df
Eric Ricketts <ericricketts@Erics-MacBook-Air.local>
2022-01-10 14:38:53 -0500

    got the new version to pass the Leetcode submission, but my memory usage and runtimes were very poor, only better than 4% of the submissions

--- java/easy/minimun-absolute-difference/src/main/java/Solution.java
+++ java/easy/minimun-absolute-difference/src/main/java/Solution.java
@@ -2,53 +2,26 @@ import java.util.*;
 
 public class Solution {
     public List<List<Integer>> minimumAbsDifference(int[] arr) {
-        List<List<Integer>> minimumAbsDifferenceListOfPairs = new ArrayList<>();
-        int lastIndex = arr.length - 1;
-        int[] miniDiffArray = new int[]{(int)Integer.MAX_VALUE};
-
-        for (int index = 0; index < lastIndex; index += 1) {
-            int[] arraySlice = Arrays.copyOfRange(arr, index, lastIndex + 1);
-            accumulateAbsDifferenceOfPairs(minimumAbsDifferenceListOfPairs, miniDiffArray, arraySlice);
-        }
-
-        sortListOfPairs(minimumAbsDifferenceListOfPairs);
-        return minimumAbsDifferenceListOfPairs;
+        Arrays.sort(arr);
+        return accumulatedMinDiffPairs(arr);
     }
 
-    private void accumulateAbsDifferenceOfPairs(List<List<Integer>> pairs, int[] miniDiffArray, int[] arraySlice) {
-        int firstValue = arraySlice[0];
-
-        for (int index = 1; index < arraySlice.length; index += 1) {
-            int secondValue = arraySlice[index];
-            int difference = Math.abs(secondValue - firstValue);
-            int currentMinDiff = miniDiffArray[0];
-            if (difference < currentMinDiff) {
-                miniDiffArray[0] = difference;
+    private List<List<Integer>> accumulatedMinDiffPairs(int[] arr) {
+        int minDifference = Integer.MAX_VALUE;
+        List<List<Integer>> pairs = new ArrayList<>();
+        for (int i = 0; i < arr.length - 1; i += 1) {
+            Integer[] pair = new Integer[]{arr[i], arr[i+1]};
+            int difference = arr[i+1] - arr[i];
+            if (difference < minDifference) {
+                minDifference = difference;
                 pairs.clear();
-                sortPairAndAddToList(pairs, firstValue, secondValue);
-            } else if (difference == currentMinDiff) {
-                sortPairAndAddToList(pairs, firstValue, secondValue);
+                pairs.add(Arrays.asList(pair));
+            } else if (difference == minDifference) {
+                pairs.add(Arrays.asList(pair));
             } else {
                 continue;
             }
         }
+        return pairs;
     }
-
-    private void sortListOfPairs(List<List<Integer>> pairs) {
-        Comparator<List<Integer>> comp = (a, b) -> {
-            return a.get(0).compareTo(b.get(0));
-        };
-        Collections.sort(pairs, comp);
-    }
-
-    private void sortPairAndAddToList(List<List<Integer>> pairs, int firstValue, int secondValue) {
-        Integer[] pair;
-        if (secondValue < firstValue) {
-            pair = new Integer[]{secondValue, firstValue};
-        } else {
-            pair = new Integer[]{firstValue, secondValue};
-        }
-        pairs.add(Arrays.asList(pair));
-    }
-
 }
