2035471d3795549610a49e50449724fe992bffd3
Eric Ricketts <eric_ricketts@me.com>
2022-03-07 17:55:16 -0500

    tried some variations on average salary

--- java/programming_skills/easy/src/main/java/org/example/AverageSalary.java
+++ java/programming_skills/easy/src/main/java/org/example/AverageSalary.java
@@ -2,14 +2,14 @@ package org.example;
 
 public class AverageSalary {
     public double average(int[] salary) {
-        double maxSalary = 999.0;
-        double minSalary = 1_000_001.0;
-        double sumOfSalaries = 0.0;
+        int maxSalary = 999;
+        int minSalary = 1_000_001;
+        int sumOfSalaries = 0;
         for (int oneSalary:salary) {
             if (oneSalary > maxSalary) maxSalary = oneSalary;
             if (oneSalary < minSalary) minSalary = oneSalary;
             sumOfSalaries += oneSalary;
         }
-        return (sumOfSalaries - (maxSalary + minSalary)) / (salary.length - 2);
+        return (double)(sumOfSalaries - (maxSalary + minSalary)) / (salary.length - 2);
     }
 }
