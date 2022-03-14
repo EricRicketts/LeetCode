524711ed524936214b53647eba602b0efe99a027
EricRicketts <eric_ricketts@me.com>
2022-03-14 17:51:13 -0400

    I had to look at the answer my algorithm was totally messed up for Largest Perimeter Triangle

--- java/programming_skills/easy/src/main/java/org/example/LargestPerimeterTriangle.java
+++ java/programming_skills/easy/src/main/java/org/example/LargestPerimeterTriangle.java
@@ -1,15 +1,26 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class LargestPerimeterTriangle {
+
     public int largestPerimeter(int[] nums) {
-        int perimeter = 0, longest = 0, middle = 0, shortest = 0, index = 0;
-        int numberOfSides = nums.length;
-        while (index < numberOfSides) {
-            if (index == 0) {
-                longest = nums[index];
-            } else if (index == 1) {
+        Arrays.sort(nums);
+        for (int index = nums.length - 3; index >= 0; --index) {
+            if (nums[index] + nums[index + 1] > nums[index + 2]) {
+                return nums[index] + nums[index + 1] + nums[index + 2];
             }
         }
-        return perimeter;
+        return 0;
     }
 }
+/*
+    the key here was to sort the array and work backwards, I never thought of this.  I need to think about just
+    getting the program to work rather than optimizing for performance.
+
+    if we sort and work backwards we are guaranteed to get the largest numbers in the back of the array.  Then we
+    progress backwards starting from the third element from the end.  We then advance backwards one element at a time
+    guaranteeing we will always have the three largest to work with.  Once the condition is met we can exit the loop.
+
+    My original solution was too complicated because I was thinking about making just one pass through the array.
+*/
--- java/programming_skills/easy/src/test/java/org/example/LargestPerimeterTriangleTest.java
+++ java/programming_skills/easy/src/test/java/org/example/LargestPerimeterTriangleTest.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -13,33 +14,39 @@ class LargestPerimeterTriangleTest {
         largestPerimeterTriangle = new LargestPerimeterTriangle();
     }
 
+//    @Disabled
     @Test
     void largestPerimeterWithValidSides() {
         assertEquals(5, largestPerimeterTriangle.largestPerimeter(new int[]{2, 1, 2}));
     }
 
+//    @Disabled
     @Test
     void largestPerimeterWithNonValidSides() {
         assertEquals(0, largestPerimeterTriangle.largestPerimeter(new int[]{1, 2, 1}));
     }
 
+//    @Disabled
     @Test
     void setLargestPerimeterWithAllEqualSides() {
         assertEquals(3, largestPerimeterTriangle.largestPerimeter(new int[]{1, 1, 1, 1, 1, 1}));
     }
 
+//    @Disabled
     @Test
-    void setLargestPerimeterTriangleWithALargeRandomeArrayOfSides() {
+    void setLargestPerimeterTriangleWithALargeRandomArrayOfSides() {
         int[] sides = new int[]{7, 10, 12, 5, 6, 13, 1, 14, 8, 9};
         assertEquals(39, largestPerimeterTriangle.largestPerimeter(sides));
     }
 
+//    @Disabled
     @Test
     void setLargestPerimeterTriangleFixFailedTest() {
         int[] sides = new int[]{3, 6, 2, 3};
         assertEquals(8, largestPerimeterTriangle.largestPerimeter(sides));
     }
 
+//    @Disabled
     @Test
     void fixAnotherFailedTest() {
         int[] sides = new int[]{1,2,2,4,18,8};
