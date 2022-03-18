c7ec0dfdea4e7993b6c5a8f872c09397eb8d9190
EricRicketts <eric_ricketts@me.com>
2022-03-18 18:38:10 -0400

    bring leetcode up to date

--- java/programming_skills/easy/string_swap/src/main/java/org/example/Solution.java
+++ java/programming_skills/easy/string_swap/src/main/java/org/example/Solution.java
@@ -1,22 +1,34 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Solution {
     public boolean areAlmostEqual(String s1, String s2) {
-        if (s1.equals(s2)) return true;
-        if (s1.length() == 1) return false;
-        int lastIndex = s1.length() - 1;
-
-        for (int outerIndex = 0; outerIndex < lastIndex; outerIndex++) {
-            for (int innerIndex = outerIndex; innerIndex < lastIndex; innerIndex++) {
-                char[] chars = s2.toCharArray();
-                int nextIndex = innerIndex + 1;
-                char temp = chars[outerIndex];
-                chars[outerIndex] = chars[nextIndex];
-                chars[nextIndex] = temp;
-                String stringWithSwappedChars = String.valueOf(chars);
-                if (s1.equals(stringWithSwappedChars)) return true;
+        int differenceCount = 0;
+        int lengthOfString = s1.length();
+        boolean matchingLetterPairs = false;
+        List<Character> letterPairs = new ArrayList<>();
+        for (int index = 0; index < lengthOfString; index++) {
+            char firstStringLetter = s1.charAt(index);
+            char secondStringLetter = s2.charAt(index);
+            if (firstStringLetter != secondStringLetter) {
+                differenceCount += 1;
+                letterPairs.add(firstStringLetter);
+                letterPairs.add(secondStringLetter);
             }
         }
-        return false;
+
+        if (letterPairs.size() > 2) {
+            matchingLetterPairs = (letterPairs.get(0) == letterPairs.get(3)) &&
+                (letterPairs.get(1) == letterPairs.get(2));
+        }
+        return (differenceCount == 0) || (s1.length() == 1 && (s1.charAt(0) == s2.charAt(0))) ||
+            (differenceCount == 2 && matchingLetterPairs);
     }
 }
+/*
+    Algorithm
+    1.  Only iterate over the string one time.
+    2.  Set up an ArrayList
+*/
